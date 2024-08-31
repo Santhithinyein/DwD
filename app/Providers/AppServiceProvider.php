207720c88e017284d03c97200b8e85ceec2f3aa8
Santhi <santhithinyein@gmail.com>
2024-08-31 13:51:11 +0630

    adding gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+// use Illuminate\Support\Facades\Gate;
+
+// use Illuminate\Auth\Access\Gate;
+
+// use Illuminate\Contracts\Auth\Access\Gate;
+
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +26,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('superadmin', function ($user) {
+            return $user->userType->type === 'superadmin'; // Adjust based on your role setup
+        });
+
+        Gate::define('admin', function ($user) {
+            return $user->userType->type === 'admin'; // Adjust based on your role setup
+        });
     }
 }
